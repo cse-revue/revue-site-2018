e26c173baaf1bc20462add3732d65c29d205b54e
Spencer <spencer.monro9@gmail.com>
2020-01-23 19:49:51 +1100

    Added sponsos and such

--- src/screens/Sponsors.js
+++ src/screens/Sponsors.js
@@ -4,13 +4,22 @@ import arc from '../img/Arc-Clubs-Logo-Black-Transparent.png';
 import coffeeOnCampus from '../img/CoffeeOnCampus.jpg';
 import cseSchool from '../img/cse-school.png';
 import faastprint from '../img/faastprint.png';
+import remnant1 from '../img/remnant1.png';
+import remnant2 from '../img/remnant2.png';
+import remnant3 from '../img/remnant3.png';
 
 export default class Sponsors extends Component {
   render() {
     return (
-      <div className="row sponsors-page">
-        <img src={cseSchool} alt="UNSW School of CSE"/>
-        <img src={faastprint} alt="Faastprint"/>
+      <div>
+        <div className="row sponsors-page">
+          <img src={cseSchool} alt="UNSW School of CSE"/>
+          <img src={arc} alt="Arc"/>
+        </div>
+        <div className="row sponsors-page">
+          <img src={remnant3} alt="Remnant"/>
+          <img src={faastprint} alt="Faastprint"/>
+        </div>
       </div>
     );
   }
